bef61cc40614cb7d4f16775a5b9f1f3f12c41237
FranciscoCarbonell <38758320+FranciscoCarbonell@users.noreply.github.com>
2020-05-06 19:48:28 +0200

    init commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get remove -y cython
+sudo aptitude install -y python3-dev net-tools build-essential adb aidl openssh-server python3-pip
+sudo aptitude install -y git zip unzip openjdk-8-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake
+sudo aptitude install -y mesa-common-dev libglu1-mesa-dev freeglut3-dev libffi-dev
+
+sudo python3 -m pip install buildozer
+sudo python3 -m pip install -U cython==0.29.6
+sudo python3 -m pip install virtualenv
