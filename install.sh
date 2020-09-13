54572f700f44028c96d0cc09c6d5f2a6c68ee5ae
FranciscoCarbonell <38758320+FranciscoCarbonell@users.noreply.github.com>
2020-09-13 16:07:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 #sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get remove -y cython
-sudo aptitude install -y python3-dev net-tools build-essential adb aidl openssh-server python3-pip
+sudo aptitude install -y python3-dev net-tools build-essential adb aidl openssh-server python3-pip xclip
 sudo aptitude install -y git zip unzip openjdk-8-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake
 sudo aptitude install -y mesa-common-dev libglu1-mesa-dev freeglut3-dev libffi-dev, libssl-dev
